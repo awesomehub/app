61243391fdb5612f1b4b4b375193b75ee44366e0
Mohamed Elkholy <mkh117@gmail.com>
2025-10-21 01:56:32 +0400

    fix(deploy): adjust headers and redirects
    Signed-off-by: Mohamed Elkholy <mkh117@gmail.com>

--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -24,10 +24,12 @@ async function main() {
       'X-Content-Type-Options: nosniff',
       'Referrer-Policy: same-origin',
     ],
-    '': [
+    'index.html': [
       'Cache-Control: public, max-age=0, must-revalidate',
+      'Link: <https://awesomehub.github.io>; rel=preconnect',
+      'Link: <https://www.googletagmanager.com>; rel=preconnect',
+      'Link: <https://www.google-analytics.com>; rel=preconnect',
       'Link: <https://fonts.gstatic.com>; rel=preconnect',
-      'Link: <https://fonts.gstatic.com>; rel=dns-prefetch',
     ],
     'static/*': [
       'Cache-Control: public, max-age=31536000, immutable'
@@ -42,7 +44,7 @@ async function main() {
       headers.set(asset, [
         'Cache-Control: public, max-age=31536000, immutable'
       ])
-      headers.get('').push(
+      headers.get('index.html').push(
         `Link: </${asset}>; rel=preload; as=${ext === '.css' ? 'style' : 'script'}`
       )
       log('info', 'webpack:asset', asset)
@@ -58,8 +60,16 @@ async function main() {
   fs.writeFileSync(headersFile, headersData.join('\n\n') + '\n')
   log('info', '_headers', headersFile, 'done!')
 
+  const redirects = [
+    { from: '/list/selfhosted/internet-of-things-iot', to: '/list/selfhosted/iot-platforms' },
+    { from: '/list/selfhosted/proxy', to: '/list/selfhosted/networking%2Fproxy' },
+    { from: '/list/selfhosted/money-budgeting', to: '/list/selfhosted/finance-budgeting' },
+    { from: '/*', to: '/index.html', status: 200 },
+  ]
+
+  const redirectsData = redirects.map(({ from, to, status }) => `${from}    ${to}    ${status ?? 301}`)
   const redirectsFile = dist('_redirects')
-  fs.writeFileSync(redirectsFile, '/*    /index.html   200\n')
+  fs.writeFileSync(redirectsFile, redirectsData.join('\n') + '\n')
   log('info', '_redirects', redirectsFile, 'done!')
 }
 
