6e8a593f734a94e3838d9d6892c5e038306fdaec
Mohamed Elkholy <mkh117@gmail.com>
2025-10-26 18:51:25 +0400

    fix(app): fix redirect issue
    Signed-off-by: Mohamed Elkholy <mkh117@gmail.com>

--- src/app/lists/services/resolvers/list-category.resolver.ts
+++ src/app/lists/services/resolvers/list-category.resolver.ts
@@ -4,8 +4,7 @@ import type { List, ListCategory } from '../../state'
 
 export const listCategoryDataResolver: ResolveFn<ListCategory | boolean> = (route: ActivatedRouteSnapshot) => {
   const router = inject(Router)
-  const segments = route.url.map((segment) => segment.path)
-  const categoryPath = segments.join('/')
+  const categoryPath = route.url.map((segment) => segment.path).join('/')
 
   const list: List = route.parent.data['list']
   const category = list.cats.find((c) => c.path === categoryPath)
@@ -17,7 +16,7 @@ export const listCategoryDataResolver: ResolveFn<ListCategory | boolean> = (rout
   const categoryPathEncoded = route.url.map((segment) => segment.toString()).join('/')
   if (categoryPath !== categoryPathEncoded) {
     // needed to redirect legacy category urls to new ones
-    router.navigate(['list', list.id, ...segments], {
+    router.navigate(['list', list.id, ...categoryPath.split('/')], {
       queryParams: route.queryParams,
       fragment: route.fragment,
       replaceUrl: true,
