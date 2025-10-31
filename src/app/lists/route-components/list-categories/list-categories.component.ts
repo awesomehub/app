5d01e8e1478caa33c4ff344e963a7cccd15ec44f
Mohamed Elkholy <mkh117@gmail.com>
2025-10-31 18:38:58 +0400

    fix(app): fx drawer scroll behaviour
    Signed-off-by: Mohamed Elkholy <mkh117@gmail.com>

--- src/app/lists/route-components/list-categories/list-categories.component.ts
+++ src/app/lists/route-components/list-categories/list-categories.component.ts
@@ -7,6 +7,16 @@ import type { List } from '../../state'
 @Component({
   styleUrls: ['./list-categories.component.css'],
   template: `
+    @if (list.copyright) {
+      <div class="list-copyright">
+        <button id="list-copyright-btn" class="mdl-button mdl-js-button">
+          <ah-svg key="info" class="icon icon-24" />
+        </button>
+        <div for="list-copyright-btn" class="mdl-menu mdl-js-menu mdl-menu--unaligned">
+          <ah-markdown [value]="list.copyright" />
+        </div>
+      </div>
+    }
     <nav class="list-navigation mdl-navigation">
       <a
         class="mdl-navigation__link"
@@ -25,18 +35,13 @@ import type { List } from '../../state'
       >
         Browse All
       </a>
-      @if (list.copyright) {
-        <div class="list-copyright">
-          <button id="list-copyright-btn" class="mdl-button mdl-js-button">
-            <ah-svg key="info" class="icon icon-24" />
-          </button>
-          <div for="list-copyright-btn" class="mdl-menu mdl-js-menu mdl-menu--unaligned">
-            <ah-markdown [value]="list.copyright" />
-          </div>
-        </div>
-      }
       <span class="divider"></span>
-      <ah-list-categories [list]="list" [parent]="0" (navigate)="app.toggleDrawer()" />
+      <ah-list-categories
+        [list]="list"
+        [parent]="0"
+        (navigate)="app.toggleDrawer()"
+        style="height: calc(100vh - 190px); overflow: auto;"
+      />
     </nav>
   `,
   encapsulation: ViewEncapsulation.None,
